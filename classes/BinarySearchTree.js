b2fd1cd1d2a3198241eb7724b2b8aa3d119c0c04
Graig Janssen <graig.janssen@gmail.com>
2023-03-23 18:19:24 -0400

    bst remove iterative part 1

--- classes/BinarySearchTree.js
+++ classes/BinarySearchTree.js
@@ -54,5 +54,60 @@ class BinarySearchTree {
         pointer = value > pointer.value ? pointer.right : pointer.left;
       }
     }
+    return null;
+  }
+
+  remove(value) {
+    // Empty tree
+    if (!this.root) {
+      return;
+    }
+
+    let current = this.root;
+    let parent = null;
+
+    // Find the node
+    while (current !== null && current.value !== value) {
+      parent = current;
+      current = value < current.value ? current.left : current.right;
+    }
+
+    // Node not found
+    if (!current) {
+      console.log(`Could not find ${value} in that Binary Search Tree`);
+      return this.root;
+    }
+
+    /** Now we can assume we found the node, so we get into the hairy logic 
+     Break down the cases in terms of possible children: 0, 1 or 2
+     But also have to consider if the node we're deleting is the root or not 
+    */
+
+    // Current is a leaf node with no children
+    if (current.left === null && current.right === null) {
+      // Leaf with no parent (one node BST)
+      if (parent === null) {
+        this.root = null;
+      // Regular leaf scenario  
+      } else if (current === parent.left) {
+        parent.left = null;
+      } else {
+        parent.right = null;
+      }
+    // One null child  
+    } else if (current.left === null || current.right === null) {
+      const newNode = current.left || current.right;
+      // Root node
+      if (parent === null) {
+        this.root = null;
+      } else if (current === parent.left) {
+        parent.left = newNode;
+      } else {
+        parent.right = newNode;
+      }
+    // Two children  
+    } else {
+
+    }
   }
 }
