555e08108b7ac7acbaac21bae3ef3edcd325a7c7
Graig Janssen <graig.janssen@gmail.com>
2023-04-14 10:30:14 -0400

    fibonacci recursive exponential

--- exercises/fibonacci.js
+++ exercises/fibonacci.js
@@ -1,8 +1,18 @@
 // n: index of fibonacci sequence
+// Iterative: O(n)
 function fibIterative(n) {
   const fibArr = [0, 1];
   while (fibArr.length <= n) {
     fibArr.push(fibArr[fibArr.length - 1] + fibArr[fibArr.length - 2]);
   }
   return fibArr[n];
+}
+
+// Recursive: O(2^n) (exponential)
+function fibRecursive(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fibRecursive(n - 1) + fibRecursive(n - 2);
 }
